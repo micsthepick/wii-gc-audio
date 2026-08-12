00523128cbb68bf7521fccda3def54f9ed78510b
micsthepick <11528421+micsthepick@users.noreply.github.com>
2026-08-12 13:34:08 +1000

    double buffered audio

--- source/audio.c
+++ source/audio.c
@@ -8,24 +8,42 @@
 u32 underruns = 0;
 
 #define AUDIO_SAMPLES 7680
+#define AUDIO_BUFFER_COUNT 2
 
-static s16 audio_buffer[AUDIO_SAMPLES * 2]
+static s16 audio_buffers[AUDIO_BUFFER_COUNT][AUDIO_SAMPLES * 2]
     __attribute__((aligned(32)));
 
-static void audio_fill_buffer(void)
+static unsigned next_audio_buffer;
+
+static void audio_prepare_buffer(unsigned index)
 {
+    s16 *buffer = audio_buffers[index];
+
     /*
      * Anything not supplied by the FIFO is silence.
      */
-    memset(audio_buffer, 0, sizeof(audio_buffer));
+    memset(buffer, 0, sizeof(audio_buffers[index]));
 
-    unsigned got = fifo_read(audio_buffer, AUDIO_SAMPLES);
+    unsigned got = fifo_read(buffer, AUDIO_SAMPLES);
 
     if (got < AUDIO_SAMPLES) {
         underruns++;
     }
 
-    DCFlushRange(audio_buffer, sizeof(audio_buffer));
+    DCFlushRange(buffer, sizeof(audio_buffers[index]));
+}
+
+static void audio_dma_callback(void)
+{
+    unsigned index = next_audio_buffer;
+
+    audio_prepare_buffer(index);
+    AUDIO_InitDMA(
+        (u32)audio_buffers[index],
+        sizeof(audio_buffers[index])
+    );
+
+    next_audio_buffer = (index + 1) % AUDIO_BUFFER_COUNT;
 }
 
 void audio_init(void)
@@ -33,10 +51,15 @@ void audio_init(void)
     AUDIO_Init(NULL);
     AUDIO_SetDSPSampleRate(AI_SAMPLERATE_48KHZ);
 
-    memset(audio_buffer, 0, sizeof(audio_buffer));
-    DCFlushRange(audio_buffer, sizeof(audio_buffer));
+    memset(audio_buffers, 0, sizeof(audio_buffers));
+    DCFlushRange(audio_buffers, sizeof(audio_buffers));
 
-    AUDIO_RegisterDMACallback(audio_fill_buffer);
-    AUDIO_InitDMA((u32)audio_buffer, sizeof(audio_buffer));
+    next_audio_buffer = 1;
+
+    AUDIO_RegisterDMACallback(audio_dma_callback);
+    AUDIO_InitDMA(
+        (u32)audio_buffers[0],
+        sizeof(audio_buffers[0])
+    );
     AUDIO_StartDMA();
 }
