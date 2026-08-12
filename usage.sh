36136d619578bec85513500cdceb1fa3d47ae550
micsthepick <11528421+micsthepick@users.noreply.github.com>
2026-08-12 19:16:23 +1000

    usage.sh: clarify that audio is stereo

--- usage.sh
+++ usage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# pipe 16 bit 48KHz s16le PCM to this script
+# pipe stereo 16 bit 48KHz s16le PCM to this script
 python stream-wiigc-encoder.py "${1:-128000}" | python tastm32-stream-wiigc-stereo.py
